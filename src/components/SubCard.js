e6968020124418e09fcbbe7b1924a2487f226fd3
Khushal-Gupta <khushalgupta8@gmail.com>
2021-12-31 19:54:58 +0530

    Error in adding new card button, state not changing'

--- src/components/SubCard.js
+++ src/components/SubCard.js
@@ -2,7 +2,7 @@ import classes from './SubCard.module.css';
 
 const SubCard = (props) => {
     return <div className={classes.wrapper}>
-        This is a sub card
+        {props.state.title}
     </div>
 };
 
--- src/pages/ProjectCardList.js
+++ src/pages/ProjectCardList.js
@@ -1,13 +1,27 @@
-import ProjectCard from '../components/ProjectCard';
-import classes from './ProjectCardList.module.css';
+import { useState } from "react";
+import ProjectCard from "../components/ProjectCard";
+import { DUMMY_PROJECT_CARDS } from "../models/ProjectCardModel";
+import classes from "./ProjectCardList.module.css";
 
 const ProjectCardList = () => {
-    return <div className={classes.wrapper}>
-        <ProjectCard/>
-        <ProjectCard/>
-        <ProjectCard/>
-        <ProjectCard/>
+  const [state, setState] = useState("");
+  const addCardHandler = () => {
+    console.log("updating state");
+    console.log(DUMMY_PROJECT_CARDS);
+
+    setState();
+  };
+  return (
+    <div className={classes.wrapper}>
+      {DUMMY_PROJECT_CARDS.map((elem) => (
+        <ProjectCard
+          state={elem.state}
+          object={elem}
+          onAddCard={addCardHandler}
+        />
+      ))}
     </div>
+  );
 };
 
 export default ProjectCardList;
